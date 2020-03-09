d928078065c47104655200ffd7825af251c87712
dos62189 <dos62189@CAVD700052.deltadev.ent>
2020-03-09 18:16:25 +0530

    Added loggers in Service Layer like debug and error.

--- src/main/java/com/employee/service/EmployeService.java
+++ src/main/java/com/employee/service/EmployeService.java
@@ -1,9 +1,10 @@
 package com.employee.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,51 +18,87 @@ public class EmployeService {
 	@Autowired
 	IEmployeeRepo employeeRepo;
 
-	public List<Employee> getAllEmployees() {
-		List<Employee> alllist = new ArrayList<Employee>();
+	private static final Logger logger = LoggerFactory.getLogger(EmployeService.class);
 
-		alllist = employeeRepo.findAll();
+	public List<Employee> getAllEmployees() throws EmployeeException {
+		List<Employee> alllist = null;
+		logger.debug("START :::: Get all employee details from datastore");
+		try {
+			alllist = employeeRepo.findAll();
+		} catch (Exception exception) {
+			logger.error("Issue while fetching all employee details from datastore" + exception);
+			throw new EmployeeException("There are no employees in the datastore");
+		}
 
 		return alllist;
 	}
 
 	public Employee findEmployee(String employeeNumber) throws EmployeeException {
-		Optional<Employee> employee = employeeRepo.findById(employeeNumber);
 
-		if (!employee.isPresent()) {
+		Optional<Employee> employeeOptional = Optional.empty();
+		logger.debug("START :::: Get an employee details from datastore based on given employeeNumber");
+		try {
+			employeeOptional = employeeRepo.findById(employeeNumber);
 
-			throw new EmployeeException("employee is not found with id:" + employeeNumber);
+			if (!employeeOptional.isPresent()) {
+
+				throw new EmployeeException("employee is not found with id:" + employeeNumber);
+			}
+		} catch (Exception exception) {
+			logger.error("Issue while fetching an employee details from datastore" + exception);
+			throw new EmployeeException(
+					"There is no employee in the datastore with given employeeNumber" + employeeNumber);
 		}
-		Employee record = employee.get();
-		return record;
+
+		return employeeOptional.get();
 	}
 
-	public String updateOrSaveEmployee(Employee employee) {
-		Optional<Employee> employees = employeeRepo.findById(employee.getEmployeeNumber());
-		String result = null;
-		
-		if (employees.isPresent()) {
-			Employee existingEmployee = employees.get();
+	public String updateOrSaveEmployee(Employee employee) throws EmployeeException {
 
-			existingEmployee.setEmployeeName(employee.getEmployeeName());
-			existingEmployee.setEmployeeAddress(employee.getEmployeeAddress());
-			existingEmployee.setDepartmentName(employee.getDepartmentName());
-			employeeRepo.save(existingEmployee);
-			result = "Updated";
-		} else {
-			employeeRepo.save(employee);
-			result = "Created";
+		Optional<Employee> employeesOptional = Optional.empty();
+		String result = null;
+
+		logger.debug("START :::: Update / Create an employee details in datastore based on given employee details");
+		try {
+			employeesOptional = employeeRepo.findById(employee.getEmployeeNumber());
+
+			if (employeesOptional.isPresent()) {
+				Employee existingEmployee = employeesOptional.get();
+
+				existingEmployee.setEmployeeName(employee.getEmployeeName());
+				existingEmployee.setEmployeeAddress(employee.getEmployeeAddress());
+				existingEmployee.setDepartmentName(employee.getDepartmentName());
+				employeeRepo.save(existingEmployee);
+				result = "Updated";
+			} else {
+				employeeRepo.save(employee);
+				result = "Created";
+			}
+		} catch (Exception exception) {
+			logger.error("Issue while creating / updating an employee details in datastore" + exception);
+			throw new EmployeeException(
+					"Issue while creating or updating the employee details" + employee.getEmployeeNumber());
 		}
+
 		return result;
 	}
 
 	public void deleteEmployeeById(String employeeNumber) throws EmployeeException {
-		Optional<Employee> employee = employeeRepo.findById(employeeNumber);
 
-		if (employee.isPresent()) {
-			employeeRepo.deleteById(employeeNumber);
-		} else {
-			throw new EmployeeException("no employee record found to delete with the id:" + employeeNumber);
+		Optional<Employee> employeeOptional = Optional.empty();
+		logger.debug("START :::: Delete an employee in datastore based on given employee number");
+
+		try {
+			employeeOptional = employeeRepo.findById(employeeNumber);
+
+			if (employeeOptional.isPresent()) {
+				employeeRepo.deleteById(employeeNumber);
+			} else {
+				throw new EmployeeException("no employee record found to delete with the id:" + employeeNumber);
+			}
+		} catch (Exception exception) {
+			logger.error("Issue while deleting an employee in datastore based on given employee number" + exception);
+			throw new EmployeeException("Issue while deleting the employee details" + employeeNumber);
 		}
 
 	}
